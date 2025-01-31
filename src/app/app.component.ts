30348474d2fd711aeddc064321332a1994ec9dc5
Johan Montero <johan00712@gmail.com>
2025-01-31 16:13:54 -0500

    JMontero - Inicio front login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export  class AppComponent {
   title = 'unidades_suprimidas ';
 }
